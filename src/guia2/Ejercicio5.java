c36b7aa0cb38ba81ea4f07c68533002593755c38
Alex <gbenincasa042@gmail.com>
2023-08-10 02:23:08 -0300

    Ejercicio 4 y archivo del 5

--- src/guia2/Ejercicio5.java
+++ src/guia2/Ejercicio5.java
@@ -9,29 +9,13 @@ import java.util.Scanner;
  *
  * @author gbeni
  */
-public class Ejercicio4 {
+public class Ejercicio5 {
     
     public static void main(String[] args){
     
-        String a ;
         Scanner s = new Scanner(System.in);
         
-        System.out.println("Escriba una frase o palabra ");
-        
-        a = s.nextLine();
-        
-        if(a.substring(0, 1).equals("A")){
-        
-            System.out.println("Correcto");
-            
-        }else{
-        
-            System.out.println("Incorrecto");
-        
-        }
-    
     
     }
-    
     
 }
