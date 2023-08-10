fa2058ae16bd92426214a6ddf6e695ef74ee0f0e
Alex <gbenincasa042@gmail.com>
2023-08-10 02:12:24 -0300

    ejercicio 1 y archivo del 4

--- src/guia2/Ejercicio1.java
+++ src/guia2/Ejercicio1.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package guia2;
-
+import java.util.Scanner;
 /**
  *
  * @author gbeni
@@ -13,8 +13,23 @@ public class Ejercicio1 {
     
     public static void main(String[] args){
     
+        int a ;
+        Scanner s = new Scanner(System.in);
         
-    
+        System.out.println("ingrese un numero");
+        
+        a = s.nextInt();
+        
+        if(a%2 == 0){
+        
+            System.out.println("su numero es par");
+        
+        }else{
+        
+            System.out.println("su numero es impar");
+            
+        }
+        
     }
     
 }
