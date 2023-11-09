545654a953be993c27d331cf4e6c5200cfb6a485
Alex <gbenincasa042@gmail.com>
2023-11-09 20:16:30 -0300

    archivo del ejercicio 6

---
+++ src/guia4/Ejercicio6.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package guia4;
+
+/*
+Un cuadrado mágico 3 x 3 es una matriz 3 x 3 formada por números del 1 al 9 
+donde la suma de sus filas, sus columnas y sus diagonales son idénticas. 
+Crear un programa que permita introducir un cuadrado por teclado y determine 
+si este cuadrado es mágico o no. El programa deberá comprobar que los números 
+introducidos son correctos, es decir, están entre el 1 y el 9
+ */
+/**
+ *
+ * @author gbeni
+ */
+public class Ejercicio6 {
+
+    public static void main(String[] args) {
+
+        //usar contadores
+    }
+
+}
