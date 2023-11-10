02a26cd064edf1bc1b78c6d786c182b155bf030c
Alex <gbenincasa042@gmail.com>
2023-11-09 21:56:04 -0300

    ejercicio 6 casi hecho

--- src/guia4/Ejercicio6.java
+++ src/guia4/Ejercicio6.java
@@ -4,6 +4,8 @@
  */
 package guia4;
 
+import java.util.Scanner;
+
 /*
 Un cuadrado mágico 3 x 3 es una matriz 3 x 3 formada por números del 1 al 9 
 donde la suma de sus filas, sus columnas y sus diagonales son idénticas. 
@@ -20,13 +22,133 @@ public class Ejercicio6 {
     public static void main(String[] args) {
 
         //usar contadores
+        int[][] matriz = new int[3][3];
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("ingrese los valores de la matriz");
+
+        for (int i = 0; i < matriz.length; i++) {
+
+            for (int j = 0; j < matriz[0].length; j++) {
+
+                System.out.print("\nM[" + i + "]" + "[" + j + "]" + " = ");
+                matriz[i][j] = s.nextInt();
+
+            }
+        }
+
+        int contFila = 0;
+        int contelem = 0;
+        int fila1 = 0;
+        int fila2 = 0;
+        int fila3 = 0;
+
+        int columna1 = 0;
+        int columna2 = 0;
+        int columna3 = 0;
+
+        int diagonalP = 0;
+
+        for (int[] fila : matriz) {
+            
+            contelem = 0;
+
+            for (int elem : fila) {
+
+                if (contelem == contFila) {
+
+                    diagonalP += elem;
+
+                }
+
+                switch (contFila) {
+
+                    case 0:
+
+                        fila1 += elem;
+
+                        break;
+
+                    case 1:
+
+                        fila2 += elem;
+
+                        break;
+
+                    case 2:
+
+                        fila3 += elem;
+
+                        break;
+
+                    default:
+
+                        break;
+                }
+                
+                
+                 switch (contelem) {
+
+                    case 0:
+
+                        columna1 += elem;
+
+                        break;
+
+                    case 1:
+
+                        columna2 += elem;
+
+                        break;
+
+                    case 2:
+
+                        columna3 += elem;
+
+                        break;
+
+                    default:
+
+                        break;
+                }
+
+                contelem++;
+
+            }
+
+            contFila++;
+
+        }
         
-        int[][] matriz = new int[4][4];
-        int[][] matriz2 = new int[4][4];
+        System.out.println("suma de los elementos por filas, columnas y diagonal principal");
         
-        System.out.println("");
+        System.out.println(columna1);
+        System.out.println(columna2);
+        System.out.println(columna3);
+
+        System.out.println(fila1);
+        System.out.println(fila2);
+        System.out.println(fila3);
+
+        System.out.println(diagonalP);
         
+        String aux;
         
+        for (int[] fila : matriz) {
+
+            aux = "";
+
+            for (int elem : fila) {
+
+                aux += "  " + elem;
+
+            }
+
+            System.out.println(aux);
+
+        }
+
     }
 
 }
