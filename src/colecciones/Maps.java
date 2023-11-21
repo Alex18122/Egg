5bbf8033a17419e7600168ace41f050212fe32c3
Alex <gbenincasa042@gmail.com>
2023-11-21 04:43:09 -0300

    mapas ejemplos

--- src/colecciones/Maps.java
+++ src/colecciones/Maps.java
@@ -4,6 +4,10 @@
  */
 package colecciones;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
 /**
  *
  * @author gbeni
@@ -12,7 +16,41 @@ public class Maps {
     
     public static void main(String[] args) {
         
+        HashMap<Integer,String> personas = new HashMap<>();
+        HashSet<String> hashsetprueba = new HashSet<>();
         
+        hashsetprueba.add("hola");
+        hashsetprueba.add("hola2");
+        hashsetprueba.add("hola3");
+        
+        personas.put(44438086,"Gabriel Benincasa");
+        
+        //con Entry
+        
+        for (Map.Entry<Integer, String> entry : personas.entrySet()) {
+            
+            System.out.println("DNI : " + entry.getKey() + "\nNombre : " + entry.getValue());
+            
+        }
+        
+        // Sin Entry
+        
+        for (Integer dni : personas.keySet()) {
+            
+            System.out.println("DNI : " + dni);
+            
+        }
+        
+        for (String nombre : personas.values()) {
+            
+            System.out.println("Nombre : " + nombre);
+            
+        }
+        
+        for (String string : hashsetprueba) {
+            
+            System.out.println(string);
+        }
         
         
     }
